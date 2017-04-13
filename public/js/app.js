c1d08e85cc7fe916f6609ab7979c58b8023fe6dc
Atila Romero <atilaromero@gmail.com>
2017-04-13 12:48:38 -0300

    autorefresh every 3 seconds

--- public/js/app.js
+++ public/js/app.js
@@ -77,8 +77,10 @@
     }
   }
 
-  function MainController($scope, CONFIG, api, $mdMedia, $mdDialog, $log, _) {
+  function MainController($scope, CONFIG, api, $mdMedia, $mdDialog, $log, $interval, _) {
     var mc = this;
+
+    mc.updater = null;
 
     mc.selectedModelName = null;
 
@@ -173,7 +175,12 @@
 
       mc.gridOptions.columnDefs = createGridColumns(mc.selectedSchema);
 
+      $interval.cancel(mc.updater)
+      mc.updater = $interval(function () {
+        getModelData(mc.selectedModelName, mc.selectedSchema);
+      }, 3000)
       getModelData(mc.selectedModelName, mc.selectedSchema);
+
     }
 
     function createGridColumns(schema) {
